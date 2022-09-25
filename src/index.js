ae9f0ad74d5ad0ec9307a7e19a5c478c38495cc6
vitorgongora <41486681+vitorgongora@users.noreply.github.com>
2022-09-25 19:55:54 -0300

    Initial RH pages implementation (#1)
    * add header/sider nav layouts
    
    * add circle stat
    
    * add filter buttons
    
    * add finished page
    
    * change routes
    
    * improve responsiveness
    
    * remove focus
    
    * add agendamentos
    
    * add vagas
    
    * fix typo
    
    * add administracao
    
    * add administracao
    
    * fix alignment
    
    * add site personalizavel
    
    * add color picker
    
    * add rich text editor
    
    * add informacoes da conta
    
    * add formadepagamento

--- src/index.js
+++ src/index.js
@@ -1,22 +1,85 @@
 import React from "react";
+import { render } from "react-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "antd/dist/antd.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import GlobalLayout from "./components/layout/GlobalLayout/GlobalLayout";
+import Agendamentos from "./pages/Agendamentos/Agendamentos";
+import Vagas from "./pages/Vagas/Vagas";
+import Administracao from "./pages/Adminstracao/Administracao";
+import SitePersonalizavel from "./pages/SitePersonalizavel/SitePersonalizavel";
+import InformacoesDaConta from "./pages/InformacoesDaConta/InformacoesDaConta";
+import FormaDePagamento from "./pages/FormaDePagamento/FormaDePagamento";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/admin/dashboard" element={<></>} />
-        <Route path="/admin/equipe" element={<></>} />
+        <Route
+          path="/rh/dashboard"
+          element={
+            <GlobalLayout>
+              <Dashboard />
+            </GlobalLayout>
+          }
+        />
+        <Route
+          path="/rh/agendamentos"
+          element={
+            <GlobalLayout>
+              <Agendamentos />
+            </GlobalLayout>
+          }
+        />
+        <Route
+          path="/rh/vagas"
+          element={
+            <GlobalLayout>
+              <Vagas />
+            </GlobalLayout>
+          }
+        />
+        <Route
+          path="/rh/sitepersonalizavel"
+          element={
+            <GlobalLayout>
+              <SitePersonalizavel />
+            </GlobalLayout>
+          }
+        />
+        <Route
+          path="/rh/administracao"
+          element={
+            <GlobalLayout>
+              <Administracao />
+            </GlobalLayout>
+          }
+        />
+        <Route
+          path="/rh/perfil"
+          element={
+            <GlobalLayout>
+              <InformacoesDaConta />
+            </GlobalLayout>
+          }
+        />
+        <Route
+          path="/rh/pagamento"
+          element={
+            <GlobalLayout>
+              <FormaDePagamento />
+            </GlobalLayout>
+          }
+        />
       </Routes>
-  </BrowserRouter>
-  </React.StrictMode>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
